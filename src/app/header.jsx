89af737a5ee3039dc44222b6ecdde4603a01f64c
Jair Cornejo <jaircornejo61@gmail.com>
2024-05-09 22:42:59 -0300

    add(-): cree el componente header que se va a ver en toda la pagina.

---
+++ src/app/header.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Header(){
+    const classNameLinks="text-xl font-bold transition-all duration-200 hover:text-gray-700"
+    return(
+        <header className="h-[10vh] bg-[var(--colorPrincipal)] w-screen flex justify-around items-center fixed">
+            <a href="" className={classNameLinks}>Personajes</a>
+            <a href="/"><img src="/starWarsLogoNegro.webp" alt="" className="h-[10vh]"/></a>
+            <a href="" className={classNameLinks}>Peliculas</a>
+        </header>
+    )
+}
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 import { Montserrat } from "next/font/google";
+import Header from "./header";
 import "./globals.css";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={montserrat.className}>{children}</body>
+      <body className={montserrat.className}>
+        <Header></Header>
+        {children}
+        </body>
     </html>
   );
 }
