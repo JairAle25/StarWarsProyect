35495b13a3cc5c9466047acbfd1abd93259a29a4
Jair Cornejo <jaircornejo61@gmail.com>
2024-05-12 16:25:32 -0300

    Fix(-): mejore las cards de los personajes con iconos para que se vean los colores de ojos y sus generos

--- src/app/personajes/cardPersonaje.jsx
+++ src/app/personajes/cardPersonaje.jsx
@@ -1,15 +1,35 @@
 import Image from "next/image"
 import Link from "next/link"
 import fotoPersonajes from "./../../../public/fotoPersonajes.webp"
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+const getIconoGenero =(genero)=>{
+    if(genero=="male"){
+        return "bi bi-gender-male text-[#087cc6]"
+    }else if(genero=="female"){
+        return "bi bi-gender-female text-[#fc1563]"
+    }
+    else{
+        return "bi bi-gender-ambiguous text-[#2bb454]"
+    }
+}
 
 export default function CardPersonaje({nombre,ojos,genero}){
+    const colores = ojos.split(",");
+    const classGenero = getIconoGenero(genero)
     return(
         <>
-            <Link href="/" className="flex flex-col justify-center items-center gap-3 w-[70%] border rounded-lg py-4 px-6 transition-all duration-1000 hover:bg-gray-900">
+            <Link href="/" className="flex flex-col justify-center items-center gap-3 w-[70%] border rounded-lg py-4 px-6 transition-all duration-1000 bg-[#333333] hover:bg-gray-900">
                 <h1 className="text-[var(--colorPrincipal)] font-bold text-xl">{nombre}</h1>
                 <Image src={fotoPersonajes} alt="personaje star wars" className="rounded-md"/>
-                <p>Color de ojos: {ojos}</p>
-                {genero !== "n/a" && <p>Genero: {genero}</p>}
+                {ojos !=="unknown" && 
+                <p className="flex justify-center items-center gap-2">Color de ojos : 
+                    {colores.map((color,index)=>(
+                        <i key={index} className="bi bi-eye-fill text-2xl" style={{ color: color }}></i>
+                    ))}
+                </p>
+                }
+                {genero !== "n/a" && <p className="flex justify-center items-center gap-2">Genero: <i className={`${classGenero} text-3xl`}></i></p>}
             </Link>
         </>
     )
