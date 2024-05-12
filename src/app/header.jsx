7c0a079e18e83b775ac61625422f79eac9f397ef
Jair Cornejo <jaircornejo61@gmail.com>
2024-05-12 15:19:03 -0300

    Fix(-):arreglando los links del header y hero , en vez de etiquetas a uso el componenete Link

--- src/app/header.jsx
+++ src/app/header.jsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Header(){
     const classNameLinks="text-xl font-bold transition-all duration-200 hover:text-gray-700"
     return(
         <header className="h-[10vh] bg-[var(--colorPrincipal)] w-screen flex justify-around items-center sticky">
-            <a href="" className={classNameLinks}>Personajes</a>
-            <a href="/"><img src="/starWarsLogoNegro.webp" alt="" className="h-[10vh]"/></a>
-            <a href="/peliculas" className={classNameLinks}>Peliculas</a>
+            <Link href="./personajes" className={classNameLinks}>Personajes</Link>
+            <Link href="./"><img src="/starWarsLogoNegro.webp" alt="" className="h-[10vh]"/></Link>
+            <Link href="./peliculas" className={classNameLinks}>Peliculas</Link>
         </header>
     )
 }
--- src/app/hero.jsx
+++ src/app/hero.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Hero(){
     const classNameLinks = "bg-[var(--colorPrincipal)] text-black text-lg font-bold rounded-md border border-black px-6 py-1 transition-all duration-500 hover:bg-transparent hover:text-[var(--colorPrincipal)] hover:border-[var(--coloPrincipal)]"
@@ -8,8 +9,8 @@ export default function Hero(){
             <Image src="/starWarsLogo.svg" width={100} height={100} className="w-1/4"></Image>
             <p className="text-white text-2xl">Explora el universo de Star Wars: personajes icónicos y películas legendarias</p>
             <div className="text-white flex gap-2">
-                <a href="" className={classNameLinks}>Ver Personajes</a>
-                <a href="/peliculas" className={classNameLinks}>Ver Peliculas</a>
+                <Link href="./personajes" className={classNameLinks}>Ver Personajes</Link>
+                <Link href="./peliculas" className={classNameLinks}>Ver Peliculas</Link>
             </div>
         </main>
     )
