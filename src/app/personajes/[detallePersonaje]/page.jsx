0e3271cd3465e2294267d340beb6f21b87aa9805
Jair Cornejo <jaircornejo61@gmail.com>
2024-05-13 21:56:52 -0300

    Fix(-): mejora en los estilos de los detalles del personaje elegido

--- src/app/personajes/[detallePersonaje]/page.jsx
+++ src/app/personajes/[detallePersonaje]/page.jsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import fotoPersonajes from "./../../../../public/fotoPersonajes.webp"
+
 /*Traemos 10 personajes*/
 const getPersonajes=async(ruta)=>{
     const res = await fetch(ruta)
@@ -31,16 +34,18 @@ export default async function DetallePersonaje({params}){
     const datosPersonaje = await getPersonajeElegido(personaje)
     return(
         <>
-            <div className="text-white">
-                <p>NOMBRE : {datosPersonaje.name}</p>
-                <p>COLOR DE OJOS : {datosPersonaje.eye_color}</p>
-                <p>CUMPLEAÑOS : {datosPersonaje.birth_year}</p>
-                <p>COLOR DE PELO : {datosPersonaje.hair_color}</p>
-                <p>ALTURA : {datosPersonaje.height}</p>
-                <p>COLOR DE PIEL : {datosPersonaje.skin_color}</p>
-                <p>MASA : {datosPersonaje.mass}</p>
-                <p></p>
-            </div>
+            <section className="text-white w-[90%] mx-auto my-0 flex flex-col-reverse justify-center items-center gap-5 mt-9">
+                <Image src={fotoPersonajes} className="w-1/4"/>
+                <div className="text-xl flex flex-col gap-2 text-center">
+                    <h1 className="text-5xl text-[var(--colorPrincipal)] font-bold">{datosPersonaje.name}</h1>
+                    {datosPersonaje.eye_color!=="unknown" && <p>OJOS : <span className="font-bold">{datosPersonaje.eye_color}</span></p>}
+                    {datosPersonaje.birth_year !== "unknown" && <p>CUMPLEAÑOS : <span className="font-bold"> {datosPersonaje.birth_year}</span></p>}
+                    {datosPersonaje.hair_color!=="n/a" && <p>COLOR DE PELO : <span className="font-bold">{datosPersonaje.hair_color}</span></p>}
+                    <p>ALTURA : <span className="font-bold">{datosPersonaje.height}</span></p>
+                    <p>COLOR DE PIEL : <span className="font-bold">{datosPersonaje.skin_color}</span></p>
+                    <p>MASA : <span className="font-bold">{datosPersonaje.mass}</span></p>
+                </div>
+            </section>
         </>
     )
 }
